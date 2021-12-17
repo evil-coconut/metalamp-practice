8cecfca647d5c36acf8318fb1b9392d6625a7763
evil coconut <richard.morgan2951@yandex.ru>
2021-12-17 20:17:03 +0700

    Modified calendar

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,18 +3,53 @@ import '../../js/script.js';
 import '../../js/jquery-3.6.0.min.js';
 import AirDatepicker from 'air-datepicker';
 
+let arr = []
 
-
-
-
-let airdatepicker = new AirDatepicker('#calendar', {
+let adpArrival = new AirDatepicker('#input-arrival', {
     position: 'bottom center',
     range: true,
     buttons: ['clear'],
+    multipleDates: true,
     navTitles: {
-        days: '<strong>MMMM yyyy</strong>',    
+        days: '<p style="color: #1F2041; font-size: 19px; font-weight: bold;">MMMM yyyy</p>',    
     },
-    prevHtml: '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"/></svg>'
+    //prevHtml: "<svg width="17" height="18" viewBox="0 0 17 18" fill="none"><path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"/></svg>",
+    onSelect({date, formattedDate, datepicker}) {
+        if(formattedDate.length > 0)
+            document.getElementById('input-arrival').value = formattedDate[0];
+        if(formattedDate.length > 1)
+            document.getElementById('input-departure').value = formattedDate[1];
+        else
+            document.getElementById('input-departure').value = "";
+        arr = datepicker.selectedDates
+    },
+    onShow() {
+        apdArrival.clear();
+        adpArrival.selectDate(arr);
+    }
 });
 
-console.log('hello');
+let adpDeparture = new AirDatepicker('#input-departure', {
+    position: 'bottom center',
+    range: true,
+    buttons: ['clear'],
+    multipleDates: true,
+    navTitles: {
+        days: '<p style="color: #1F2041; font-size: 19px; font-weight: bold;">MMMM yyyy</p>',    
+    },
+    //prevHtml: "<svg width="17" height="18" viewBox="0 0 17 18" fill="none"><path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"/></svg>",
+    onSelect({date, formattedDate, datepicker}) {
+        if(formattedDate.length > 0)
+            document.getElementById('input-arrival').value = formattedDate[0];
+        if(formattedDate.length > 1)
+            document.getElementById('input-departure').value = formattedDate[1];
+        else
+            document.getElementById('input-departure').value = "";
+        arr = datepicker.selectedDates
+    },
+    onShow() {
+        adpDeparture.clear();
+        adpDeparture.selectDate(arr);
+    }
+});
+
